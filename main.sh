a60f88d189187088f027c1ca54a5652fb8799ca3
Thihan Soe <thihansoe912@gmail.com>
2025-11-10 11:04:08 +0630

    added key handle

--- main.sh
+++ main.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
+
+# ========================================================
+# is_game_active the flag that drives the main game loop
+# num_rows & num_columns are display's width
+# key is the key that user pressed
+# ========================================================
 is_game_active=0
 num_rows=10
 num_columns=50
 count=0
+key=0
 
-trap "echo -e \"\e[$((num_rows + 1))B\"; echo Clearing up the screen; sleep 3; clear; exit" INT EXIT
+
+# disable input buffering
+stty -echo -icanon time 0 min 0  
+# Clean up on quit script
+trap "echo -e \"\e[$((num_rows + 1))B\"; echo Clearing up the screen; sleep 3; stty sane ; clear; exit" INT EXIT
+
+
 
 render() {
     x=0
@@ -14,18 +27,37 @@ render() {
         y=0
         while [ $y -lt $num_columns ]
         do 
-            echo -e -n "$count"
+            echo -e -n "x"
             ((y++))
         done
         echo -e ""
         ((x++))
     done
     echo -e "\e[$((num_rows + 1))A"
-    ((count++))
 }
 
 run() {
-    render
+    key=$(dd bs=1 count=1 2>/dev/null)
+    if [ "$key" == "w" ]
+    then
+        echo "UP"
+    elif [ "$key" == "d" ]
+    then 
+        echo "RIGHT"
+    elif [ "$key" == "a" ]
+    then 
+        echo "LEFT"
+    elif [ "$key" == "s" ]
+    then 
+        echo "DOWN"
+    elif [ "$key" == "q" ]
+    then 
+        echo "QUIT"
+        QUIT
+    else
+        echo "DONT KNOW COMMAND"
+    fi
+    # render
 }
 
 while [ $is_game_active ]
@@ -33,3 +65,5 @@ do
     run
     sleep 2
 done
+
+stty sane 
